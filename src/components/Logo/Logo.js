e2ed8d4fa79e32a09b2d3c5ddb31a7f7e72d9028
Bruno Rodrigues <lpbruno12@gmail.com>
2018-08-18 20:13:20 -0300

    Front-end components

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css'
+import brain from './brain.png';
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0'>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 125, width: 125 }} >
+				 <div className="Tilt-inner pa3">
+				  <img style={{padingTop: '5px'}} alt='logo' src={brain}/> </div>
+			</Tilt>
+		</div>
+	);
+}
+
+export default Logo;
